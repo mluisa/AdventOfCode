6a0b4ce239aec9ca9c0935d25917e9a528108370
mluisa <mluisa_goncalves@hotmail.com>
2023-12-04 23:41:59 -0500

    Add AoC 2023, day 4 part 1 and 2

--- aoc_2023/helpers/file_reader.py
+++ aoc_2023/helpers/file_reader.py
@@ -4,6 +4,7 @@ from pathlib import Path
 BASE_PATH = Path(__file__).parent
 DATA_FOLDER_PATH = BASE_PATH / "../data"
 
+
 def get_lines(filename: str) -> list:
     file_path = (DATA_FOLDER_PATH / filename).resolve()
     with open(file_path, mode="rt") as file:
