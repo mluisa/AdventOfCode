95b4155f76b24867a4c0fc2f22b5a26df45474bf
mlgoncalves <mluisa_goncalves@hotmail.com>
2024-12-03 20:08:07 -0500

    day 2 completed

--- src/aoc_2024/day_2.py
+++ src/aoc_2024/day_2.py
@@ -1,41 +1,61 @@
 from src.aoc_2024.helpers import file_reader
 
+class ReportAnalyzer:
+    @classmethod
+    def get_safe_reports_count(cls, reports, with_dampener=False):
+        safe_reports = 0
+        for report in reports:
+            levels = [int(level) for level in report.split()]
+            if cls._is_safe_report(levels, with_dampener):
+                safe_reports += 1
+
+        return safe_reports
+
+    @classmethod
+    def _is_safe_report(cls, levels, with_dampener):
+        if cls._validate_safety(levels):
+            return True
+
+        if not with_dampener:
+            return False
+
+        return cls._can_become_safe_by_removing_one(levels)
+
+    @classmethod
+    def _validate_safety(cls, levels):
+        is_increasing = levels[0] < levels[1] if len(levels) > 1 else True
+
+        for i in range(1, len(levels)):
+            if is_increasing:
+                if levels[i - 1] > levels[i]:
+                    return False
+            else:
+                if levels[i - 1] < levels[i]:
+                    return False
+
+            if levels[i - 1] == levels[i] or abs(levels[i - 1] - levels[i]) not in [1, 2, 3]:
+                return False
+
+        return True
+
+    @classmethod
+    def _can_become_safe_by_removing_one(cls, levels):
+        for i in range(len(levels)):
+            modified_levels = levels[:i] + levels[i + 1:]
+            if cls._validate_safety(levels=modified_levels):
+                return True
+
+        return False
 
 class Day2:
     def __init__(self, filename: str):
         self.reports = file_reader.get_lines(filename=filename)
 
     def part_1(self):
-        return self._get_safe_reports_count()
-
-    def _get_safe_reports_count(self):
-        safe_reports = 0
-        for report in self.reports:
-            levels = [int(level) for level in report.split()]
-            if self._validate_safety(levels=levels):
-                safe_reports += 1
-
-
-        return safe_reports
-
-    def _validate_safety(self, levels):
-        is_increasing = levels[0] < levels[1]
-
-        for i in range(1, len(levels)):
-            difference = abs(levels[i - 1] - levels[i]) if len(levels) > 1 else 0
-            if difference not in [1, 2, 3]:
-                return False
-
-            if is_increasing and levels[i - 1] > levels[i]:
-                return False
-
-            if not is_increasing and levels[i - 1] < levels[i]:
-                return False
-
-        return True
+        return ReportAnalyzer.get_safe_reports_count(self.reports)
 
     def part_2(self):
-        return self._get_safe_reports_count()
+        return ReportAnalyzer.get_safe_reports_count(self.reports, with_dampener=True)
 
 
 def main():
--- test/aoc_2024/test_day_2.py
+++ test/aoc_2024/test_day_2.py
@@ -1,6 +1,7 @@
 import pytest
 
-from src.aoc_2024.day_2 import Day2
+from src.aoc_2024.day_2 import Day2, ReportAnalyzer
+
 
 # 356
 def test__part_1__should_return_2_safe_reports():
@@ -8,3 +9,39 @@ def test__part_1__should_return_2_safe_reports():
 
 def test__part_2__should_return_4_safe_reports():
     assert Day2(filename="sample_day_2.txt").part_2() == 4
+
+@pytest.mark.parametrize(
+    "levels, expected",
+    [
+        ([7,6,4,2,1], True),
+        ([1,2,7,8,9], False),
+        ([9,7,6,2,1], False),
+        ([1,3,2,4,5], False),
+        ([8,6,4,4,1], False),
+        ([1,3,6,7,9], True)
+    ]
+)
+def test__validate_safety__without_tolerate_bad_level__should_return_expected(levels, expected):
+    report_analyzer = ReportAnalyzer
+
+    result = report_analyzer._validate_safety(levels)
+
+    assert result == expected
+
+@pytest.mark.parametrize(
+    "levels, expected",
+    [
+        ([7,6,4,2,1], True),
+        ([1,2,7,8,9], False),
+        ([9,7,6,2,1], False),
+        ([1,3,2,4,5], True),
+        ([8,6,4,4,1], True),
+        ([1,3,6,7,9], True)
+    ]
+)
+def test__can_become_safe_by_removing_one__with_tolerate_bad_level__should_return_expected(levels, expected):
+    report_analyzer = ReportAnalyzer()
+
+    result = report_analyzer._can_become_safe_by_removing_one(levels)
+
+    assert result == expected
